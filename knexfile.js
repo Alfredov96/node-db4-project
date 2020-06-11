54d75f01c0d3d402df8bda7606470a137282b929
alfredo villasenor <alfredo.villasenor.aof@gmail.com>
2020-06-11 16:35:37 -0700

    downloaded knex file

---
+++ knexfile.js
@@ -0,0 +1,30 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './dev.sqlite3'
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        // runs after a connection is made to the sqlite engine
+        conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
+      },
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+};
